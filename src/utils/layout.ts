67e4ee1548855b039ff85f0df587f71e35b273a4
Tibor Rogulja <tibor.rogulja@productive.io>
2024-03-06 07:27:56 +0100

    update layout

--- src/utils/layout.ts
+++ src/utils/layout.ts
@@ -1,82 +1,11 @@
+import { PDFDocument } from 'pdf-lib';
 import Variant from '../consts/physicalPageVariant';
+import type { PageElement } from '@app/models/page-element';
 import type { DocumentPage, ElementSettings } from '../models/document-page';
 
 const getMaxHeight = (els: ElementSettings) => {
   return els.reduce((x, s) => Math.max(x, s.height), 0);
 };
-
-class Layout {
-  declare documentPage: DocumentPage;
-  declare headers: ElementSettings;
-  declare footers: ElementSettings;
-  declare backgrounds: ElementSettings;
-
-  declare headerHeight: number;
-  declare footerHeight: number;
-  declare bodyHeight: number;
-  declare headerY: number;
-  declare footerY: number;
-  declare bodyY: number;
-
-  declare pageCount?: number;
-  declare layoutPages?: LayoutPage[];
-
-  constructor(documentPage: DocumentPage, settings: ElementSettings) {
-    this.documentPage = documentPage;
-    this.headers = settings.filter((s) => s.type === 'header');
-    this.footers = settings.filter((s) => s.type === 'footer');
-    this.backgrounds = settings.filter((s) => s.type === 'background');
-    this.processHeights();
-  }
-
-  get height() {
-    return this.documentPage.height;
-  }
-
-  get width() {
-    return this.documentPage.width;
-  }
-
-  get pageCountOffset() {
-    return this.documentPage.pageCountOffset;
-  }
-
-  processHeights() {
-    this.headerHeight = getMaxHeight(this.headers);
-    this.footerHeight = getMaxHeight(this.footers);
-    this.bodyHeight = this.height - this.headerHeight - this.footerHeight;
-    this.headerY = this.height - this.headerHeight;
-    this.footerY = 0;
-    this.bodyY = this.footerHeight + 1;
-
-    if (this.bodyHeight < this.height / 3) {
-      throw new Error(
-        `Header/footer too big. Page height: ${this.height}px, header: ${this.headerHeight}px, footer: ${this.footerHeight}px, body: ${this.bodyHeight}px.`
-      );
-    }
-  }
-
-  processPages(count: number) {
-    this.pageCount = count;
-    const pageCountOffset = this.pageCountOffset;
-    if (!pageCountOffset) throw new Error('Page count offset not set');
-
-    this.layoutPages = Array.from({ length: count }, (_, i) => {
-      const start = pageCountOffset;
-      const end = pageCountOffset + count;
-      const layoutPage = new LayoutPage({
-        layout: this,
-        pageIndex: i,
-        pageNumber: i + 1 + pageCountOffset,
-        header: selectVariant(this.headers, i, start, end),
-        footer: selectVariant(this.footers, i, start, end),
-        background: selectVariant(this.backgrounds, i, start, end),
-      });
-
-      return layoutPage;
-    });
-  }
-}
 
 const findVariant = (variants: ElementSettings, condition: Variant) => {
   const res = variants.find((variant) => variant.subSelector === condition);
@@ -90,17 +19,14 @@ export const selectVariant = (
   variants: ElementSettings,
   index: number,
   offset: number,
-  end: number
+  count: number
 ) => {
   if (!variants.length) return;
 
-  const currentInSet = index + 1;
-  const currentInDocs = currentInSet + offset;
-
-  const isFirst = currentInSet === 1;
-  const isLast = currentInSet === end;
-  const isOdd = currentInDocs % 2 === 1;
-  const isEven = currentInDocs % 2 === 0;
+  const isFirst = index === 0;
+  const isLast = index === count - 1;
+  const isOdd = (index + 1 + offset) % 2 === 1;
+  const isEven = (index + 1 + offset) % 2 === 0;
 
   if (isLast) {
     return (
@@ -135,39 +61,245 @@ export const selectVariant = (
   return findVariant(variants, Variant.DEFAULT);
 };
 
+export class Layout {
+  declare documentPage: DocumentPage;
+  declare settings: ElementSettings;
+
+  declare headerHeight: number;
+  declare footerHeight: number;
+  declare bodyHeight: number;
+  declare headerY: number;
+  declare footerY: number;
+  declare bodyY: number;
+
+  declare pages?: LayoutPage[];
+
+  constructor(documentPage: DocumentPage, settings: ElementSettings) {
+    this.documentPage = documentPage;
+    this.settings = settings;
+    this.setHeights();
+  }
+
+  get headers() {
+    return this.settings.filter((s) => s.type === 'header');
+  }
+
+  get footers() {
+    return this.settings.filter((s) => s.type === 'footer');
+  }
+
+  get backgrounds() {
+    return this.settings.filter((s) => s.type === 'background');
+  }
+
+  get height() {
+    return this.documentPage.height;
+  }
+
+  get width() {
+    return this.documentPage.width;
+  }
+
+  get hasBackgroundElement() {
+    return !!this.backgrounds.length;
+  }
+
+  get needsProcessing() {
+    return !!this.pages?.some((p) => p.hasElements);
+  }
+
+  get pagesForProcessing() {
+    return this.pages?.filter((p) => p.needsProcessing);
+  }
+
+  private setHeights() {
+    this.headerHeight = getMaxHeight(this.headers);
+    this.footerHeight = getMaxHeight(this.footers);
+    this.bodyHeight = this.height - this.headerHeight - this.footerHeight;
+    this.headerY = this.height - this.headerHeight;
+    this.footerY = 0;
+    this.bodyY = this.footerHeight + 1;
+
+    if (this.bodyHeight < this.height / 3) {
+      throw new Error(
+        `Header/footer too big. Page height: ${this.height}px, header: ${this.headerHeight}px, footer: ${this.footerHeight}px, body: ${this.bodyHeight}px.`
+      );
+    }
+  }
+
+  createLayoutPages() {
+    const count = this.documentPage.pageCount;
+    if (!count || count < 1) {
+      throw new Error('Layout unable to create page, invalid page count');
+    }
+
+    const offset = this.documentPage.pageCountOffset;
+    if (!offset || offset < 0) {
+      throw new Error(
+        'Layout unable to create page, invalid page count offset'
+      );
+    }
+
+    const total = this.documentPage.owner.totalPagesNumber;
+    if (!total || total < count) {
+      throw new Error(
+        'Layout unable to create page, invalid total page number'
+      );
+    }
+
+    this.pages = Array.from({ length: count }, (_, i) => {
+      const settings = [
+        selectVariant(this.headers, i, offset, count),
+        selectVariant(this.footers, i, offset, count),
+        selectVariant(this.backgrounds, i, offset, count),
+      ].filter((el): el is ElementSettings[number] => !!el);
+
+      return new LayoutPage({
+        layout: this,
+        pageIndex: i,
+        currentPageNumber: i + 1 + offset,
+        totalPagesNumber: total,
+        settings,
+      });
+    });
+
+    // ovdje sad znam za svaku stranicu koji header, footer i background treba koristiti
+  }
+
+  process() {
+    if (!this.pages) {
+      throw new Error('Unable to process layout, no pages found');
+    }
+
+    const forProcessing = this.pages.filter((p) => p.needsProcessing);
+
+    if (forProcessing.length) {
+      // TODO: run process for every page that needs processing
+      // this includes: dom isolation, injection of numbers, printing pdf
+      // we also need to populate document-page with elements or something
+      // or maybe just keep it here, so that it is linked
+      // or maybe just do the processing from document-page?
+    }
+  }
+
+  // async process() {
+  //   if (!this.pageCount) throw new Error('Page count not set');
+  //   if (!this.pages) throw new Error('Layout pages not set');
+
+  //   for (const page of this.pages) {
+  //     if (page.needsSectionProcessing) {
+  //       await this.processPage(page);
+  //     }
+  //   }
+
+  // }
+}
+
 type LayoutPageOpts = {
   layout: Layout;
   pageIndex: number;
-  pageNumber: number;
-  header?: ElementSettings[number];
-  footer?: ElementSettings[number];
-  background?: ElementSettings[number];
+  currentPageNumber: number;
+  totalPagesNumber: number;
+  settings: ElementSettings;
 };
 
 class LayoutPage {
   declare layout: Layout;
   declare pageIndex: number;
-  declare pageNumber: number;
 
-  declare header?: ElementSettings[number];
-  declare footer?: ElementSettings[number];
-  declare background?: ElementSettings[number];
+  declare currentPageNumber: number;
+  declare totalPagesNumber: number;
+
+  declare settings: ElementSettings;
+  declare elements: PageElement[];
 
   constructor(opts: LayoutPageOpts) {
     this.layout = opts.layout;
     this.pageIndex = opts.pageIndex;
-    this.pageNumber = opts.pageNumber;
-    this.header = opts.header;
-    this.footer = opts.footer;
-    this.background = opts.background;
+    this.currentPageNumber = opts.currentPageNumber;
+    this.totalPagesNumber = opts.totalPagesNumber;
+    this.settings = opts.settings;
+    this.elements = [];
   }
-}
 
-class LayoutElement {
-  declare LayoutPage: LayoutPage;
-  declare settingsId: number;
-  declare type: 'header' | 'footer' | 'background';
-  declare subSelector: string;
-  declare hasCurrentPageNumber: boolean;
-  declare hasTotalPagesNumber: boolean;
+  get store() {
+    return this.layout.documentPage.owner.store;
+  }
+
+  get html() {
+    return this.layout.documentPage.owner.html;
+  }
+
+  get hasElements() {
+    return this.settings.length;
+  }
+
+  get hasBackgroundElement() {
+    return this.settings.some((s) => s.type === 'background');
+  }
+
+  get needsProcessing() {
+    return this.settings.length !== this.elements.length;
+  }
+
+  addElement(element: PageElement) {
+    // TODO: check if element is already added
+    // check if by adding this element we have too many elements
+    this.elements.push(element);
+  }
+
+  async process() {
+    for (const setting of this.settings) {
+      if (this.elements.some((el) => el.type === setting.type)) {
+        throw new Error('Trying to process already processed element');
+      }
+
+      // check if we can reuse the element and if it is already processed
+      if (!setting.hasCurrentPageNumber) {
+        const matchingElement = this.layout.pages?.reduce<
+          PageElement | undefined
+        >((found, page) => {
+          if (found) return found;
+
+          const matchingPage = page.settings.find(
+            (s) => s.type === setting.type && s.index === setting.index
+          );
+
+          return matchingPage
+            ? page.elements.find((el) => el.type === setting.type)
+            : undefined;
+        }, undefined);
+
+        if (matchingElement) {
+          this.addElement(matchingElement);
+          continue;
+        }
+      }
+
+      // we either can't reuse the element or it is not processed
+      await this.html.prepareSection({
+        documentPageIndex: this.layout.documentPage.index,
+        sectionType: setting.type,
+        physicalPageIndex: setting.index,
+        currentPageNumber: setting.hasCurrentPageNumber
+          ? this.currentPageNumber
+          : undefined,
+        totalPagesNumber: setting.hasTotalPagesNumber
+          ? this.totalPagesNumber
+          : undefined,
+      });
+
+      // generate the element
+      const buffer = await this.html.pdf({
+        width: this.layout.width,
+        height: setting.height,
+        transparentBg:
+          this.hasBackgroundElement && setting.type !== 'background',
+      });
+
+      const pdf = await PDFDocument.load(buffer);
+      const element = this.store.createModel(setting.type, { pdf, buffer });
+      this.addElement(element);
+    }
+  }
 }
--- test/utils/layout.test.ts
+++ test/utils/layout.test.ts
@@ -19,146 +19,146 @@ const makeSetting = (opts: MockSetting) => {
 describe('selectVariant', () => {
   test('returns correct variant from filled set', () => {
     const variants1 = [
-      makeSetting({ index: 1, subSelector: Variant.FIRST }),
-      makeSetting({ index: 2, subSelector: Variant.LAST }),
-      makeSetting({ index: 3, subSelector: Variant.ODD }),
-      makeSetting({ index: 4, subSelector: Variant.EVEN }),
-      makeSetting({ index: 5, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 11, subSelector: Variant.FIRST }),
+      makeSetting({ index: 12, subSelector: Variant.LAST }),
+      makeSetting({ index: 13, subSelector: Variant.ODD }),
+      makeSetting({ index: 14, subSelector: Variant.EVEN }),
+      makeSetting({ index: 15, subSelector: Variant.DEFAULT }),
     ];
     const variants2 = [
-      makeSetting({ index: 1, subSelector: Variant.FIRST }),
-      makeSetting({ index: 2, subSelector: Variant.LAST }),
-      makeSetting({ index: 3, subSelector: Variant.ODD }),
-      makeSetting({ index: 4, subSelector: Variant.EVEN }),
-      makeSetting({ index: 5 }),
+      makeSetting({ index: 21, subSelector: Variant.FIRST }),
+      makeSetting({ index: 22, subSelector: Variant.LAST }),
+      makeSetting({ index: 23, subSelector: Variant.ODD }),
+      makeSetting({ index: 24, subSelector: Variant.EVEN }),
+      makeSetting({ index: 25 }),
     ];
     const variants3 = [
-      makeSetting({ index: 1, subSelector: Variant.ODD }),
-      makeSetting({ index: 2, subSelector: Variant.EVEN }),
-      makeSetting({ index: 3, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 31, subSelector: Variant.ODD }),
+      makeSetting({ index: 32, subSelector: Variant.EVEN }),
+      makeSetting({ index: 33, subSelector: Variant.DEFAULT }),
     ];
     const variants4 = [
-      makeSetting({ index: 1, subSelector: Variant.FIRST }),
-      makeSetting({ index: 2, subSelector: Variant.LAST }),
-      makeSetting({ index: 3, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 41, subSelector: Variant.FIRST }),
+      makeSetting({ index: 42, subSelector: Variant.LAST }),
+      makeSetting({ index: 43, subSelector: Variant.DEFAULT }),
     ];
 
-    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(1);
-    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(4);
-    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(3);
-    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(4);
-    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(2);
+    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(11);
+    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(14);
+    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(13);
+    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(14);
+    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(12);
 
-    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(1);
-    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(3);
-    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(4);
-    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(3);
-    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(2);
+    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(21);
+    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(23);
+    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(24);
+    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(23);
+    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(22);
 
-    expect(selectVariant(variants3, 0, 10, 4)?.index).toBe(1);
-    expect(selectVariant(variants3, 1, 10, 4)?.index).toBe(2);
-    expect(selectVariant(variants3, 2, 10, 4)?.index).toBe(1);
-    expect(selectVariant(variants3, 3, 10, 4)?.index).toBe(2);
+    expect(selectVariant(variants3, 0, 10, 4)?.index).toBe(31);
+    expect(selectVariant(variants3, 1, 10, 4)?.index).toBe(32);
+    expect(selectVariant(variants3, 2, 10, 4)?.index).toBe(31);
+    expect(selectVariant(variants3, 3, 10, 4)?.index).toBe(32);
 
-    expect(selectVariant(variants4, 0, 14, 4)?.index).toBe(1);
-    expect(selectVariant(variants4, 1, 14, 4)?.index).toBe(3);
-    expect(selectVariant(variants4, 2, 14, 4)?.index).toBe(3);
-    expect(selectVariant(variants4, 3, 14, 4)?.index).toBe(2);
+    expect(selectVariant(variants4, 0, 14, 4)?.index).toBe(41);
+    expect(selectVariant(variants4, 1, 14, 4)?.index).toBe(43);
+    expect(selectVariant(variants4, 2, 14, 4)?.index).toBe(43);
+    expect(selectVariant(variants4, 3, 14, 4)?.index).toBe(42);
   });
 
   test('returns correct variant from partial set with default fallback', () => {
     const variants1 = [
-      makeSetting({ index: 1, subSelector: Variant.FIRST }),
-      makeSetting({ index: 2, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 11, subSelector: Variant.FIRST }),
+      makeSetting({ index: 12, subSelector: Variant.DEFAULT }),
     ];
     const variants2 = [
-      makeSetting({ index: 1, subSelector: Variant.LAST }),
-      makeSetting({ index: 2, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 21, subSelector: Variant.LAST }),
+      makeSetting({ index: 22, subSelector: Variant.DEFAULT }),
     ];
     const variants3 = [
-      makeSetting({ index: 1, subSelector: Variant.EVEN }),
-      makeSetting({ index: 2, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 31, subSelector: Variant.EVEN }),
+      makeSetting({ index: 32, subSelector: Variant.DEFAULT }),
     ];
     const variants4 = [
-      makeSetting({ index: 1, subSelector: Variant.ODD }),
-      makeSetting({ index: 2, subSelector: Variant.DEFAULT }),
+      makeSetting({ index: 41, subSelector: Variant.ODD }),
+      makeSetting({ index: 42, subSelector: Variant.DEFAULT }),
     ];
 
-    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(1);
-    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(2);
+    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(11);
+    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(12);
 
-    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(1);
+    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(21);
 
-    expect(selectVariant(variants3, 0, 10, 5)?.index).toBe(2);
-    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(1);
-    expect(selectVariant(variants3, 2, 10, 5)?.index).toBe(2);
-    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(1);
-    expect(selectVariant(variants3, 4, 10, 5)?.index).toBe(2);
+    expect(selectVariant(variants3, 0, 10, 5)?.index).toBe(32);
+    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(31);
+    expect(selectVariant(variants3, 2, 10, 5)?.index).toBe(32);
+    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(31);
+    expect(selectVariant(variants3, 4, 10, 5)?.index).toBe(32);
 
-    expect(selectVariant(variants4, 0, 15, 5)?.index).toBe(2);
-    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(1);
-    expect(selectVariant(variants4, 2, 15, 5)?.index).toBe(2);
-    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(1);
-    expect(selectVariant(variants4, 4, 15, 5)?.index).toBe(2);
+    expect(selectVariant(variants4, 0, 15, 5)?.index).toBe(42);
+    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(41);
+    expect(selectVariant(variants4, 2, 15, 5)?.index).toBe(42);
+    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(41);
+    expect(selectVariant(variants4, 4, 15, 5)?.index).toBe(42);
   });
 
   test('returns correct variant from partial set with undefined fallback', () => {
     const variants1 = [
-      makeSetting({ index: 1, subSelector: Variant.FIRST }),
-      makeSetting({ index: 2 }),
+      makeSetting({ index: 11, subSelector: Variant.FIRST }),
+      makeSetting({ index: 12 }),
     ];
     const variants2 = [
-      makeSetting({ index: 1, subSelector: Variant.LAST }),
-      makeSetting({ index: 2 }),
+      makeSetting({ index: 21, subSelector: Variant.LAST }),
+      makeSetting({ index: 22 }),
     ];
     const variants3 = [
-      makeSetting({ index: 1, subSelector: Variant.EVEN }),
-      makeSetting({ index: 2 }),
+      makeSetting({ index: 31, subSelector: Variant.EVEN }),
+      makeSetting({ index: 32 }),
     ];
     const variants4 = [
-      makeSetting({ index: 1, subSelector: Variant.ODD }),
-      makeSetting({ index: 2 }),
+      makeSetting({ index: 41, subSelector: Variant.ODD }),
+      makeSetting({ index: 42 }),
     ];
 
-    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(1);
-    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(2);
-    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(2);
+    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(11);
+    expect(selectVariant(variants1, 1, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 2, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 3, 0, 5)?.index).toBe(12);
+    expect(selectVariant(variants1, 4, 0, 5)?.index).toBe(12);
 
-    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(2);
-    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(1);
+    expect(selectVariant(variants2, 0, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 1, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 2, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 3, 5, 5)?.index).toBe(22);
+    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(21);
 
-    expect(selectVariant(variants3, 0, 10, 5)?.index).toBe(2);
-    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(1);
-    expect(selectVariant(variants3, 2, 10, 5)?.index).toBe(2);
-    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(1);
-    expect(selectVariant(variants3, 4, 10, 5)?.index).toBe(2);
+    expect(selectVariant(variants3, 0, 10, 5)?.index).toBe(32);
+    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(31);
+    expect(selectVariant(variants3, 2, 10, 5)?.index).toBe(32);
+    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(31);
+    expect(selectVariant(variants3, 4, 10, 5)?.index).toBe(32);
 
-    expect(selectVariant(variants4, 0, 15, 5)?.index).toBe(2);
-    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(1);
-    expect(selectVariant(variants4, 2, 15, 5)?.index).toBe(2);
-    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(1);
-    expect(selectVariant(variants4, 4, 15, 5)?.index).toBe(2);
+    expect(selectVariant(variants4, 0, 15, 5)?.index).toBe(42);
+    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(41);
+    expect(selectVariant(variants4, 2, 15, 5)?.index).toBe(42);
+    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(41);
+    expect(selectVariant(variants4, 4, 15, 5)?.index).toBe(42);
   });
 
   test('returns correct variant from single element set', () => {
-    const variants1 = [makeSetting({ index: 1, subSelector: Variant.FIRST })];
-    const variants2 = [makeSetting({ index: 1, subSelector: Variant.LAST })];
-    const variants3 = [makeSetting({ index: 1, subSelector: Variant.EVEN })];
-    const variants4 = [makeSetting({ index: 1, subSelector: Variant.ODD })];
+    const variants1 = [makeSetting({ index: 11, subSelector: Variant.FIRST })];
+    const variants2 = [makeSetting({ index: 21, subSelector: Variant.LAST })];
+    const variants3 = [makeSetting({ index: 31, subSelector: Variant.EVEN })];
+    const variants4 = [makeSetting({ index: 41, subSelector: Variant.ODD })];
 
-    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(1);
+    expect(selectVariant(variants1, 0, 0, 5)?.index).toBe(11);
     expect(selectVariant(variants1, 1, 0, 5)?.index).toBeUndefined();
     expect(selectVariant(variants1, 2, 0, 5)?.index).toBeUndefined();
     expect(selectVariant(variants1, 3, 0, 5)?.index).toBeUndefined();
@@ -168,18 +168,18 @@ describe('selectVariant', () => {
     expect(selectVariant(variants2, 1, 5, 5)?.index).toBeUndefined();
     expect(selectVariant(variants2, 2, 5, 5)?.index).toBeUndefined();
     expect(selectVariant(variants2, 3, 5, 5)?.index).toBeUndefined();
-    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(1);
+    expect(selectVariant(variants2, 4, 5, 5)?.index).toBe(21);
 
     expect(selectVariant(variants3, 0, 10, 5)?.index).toBeUndefined();
-    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(1);
+    expect(selectVariant(variants3, 1, 10, 5)?.index).toBe(31);
     expect(selectVariant(variants3, 2, 10, 5)?.index).toBeUndefined();
-    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(1);
+    expect(selectVariant(variants3, 3, 10, 5)?.index).toBe(31);
     expect(selectVariant(variants3, 4, 10, 5)?.index).toBeUndefined();
 
     expect(selectVariant(variants4, 0, 15, 5)?.index).toBeUndefined();
-    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(1);
+    expect(selectVariant(variants4, 1, 15, 5)?.index).toBe(41);
     expect(selectVariant(variants4, 2, 15, 5)?.index).toBeUndefined();
-    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(1);
+    expect(selectVariant(variants4, 3, 15, 5)?.index).toBe(41);
     expect(selectVariant(variants4, 4, 15, 5)?.index).toBeUndefined();
   });
 });
