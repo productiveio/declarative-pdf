142040b62117d9c67597c48539ceb4317a75ecab
Tibor Rogulja <tibor.rogulja@productive.io>
2024-03-06 14:50:38 +0100

    fix styling

--- src/utils/writeBuffer.ts
+++ src/utils/writeBuffer.ts
@@ -1,11 +1,14 @@
-import {open, write, close} from 'fs';
+import { open, write, close } from 'fs';
 
-export default async function writeBuffer(buffer: Buffer | Uint8Array, path: string) {
-  return new Promise(function(resolve) {
-    open(path, 'w', function(err: unknown, fd: number) {
+export default async function writeBuffer(
+  buffer: Buffer | Uint8Array,
+  path: string
+) {
+  return new Promise(function (resolve) {
+    open(path, 'w', function (err: unknown, fd: number) {
       if (err) throw err;
 
-      write(fd, buffer, 0, buffer.length, null, function(err: unknown) {
+      write(fd, buffer, 0, buffer.length, null, function (err: unknown) {
         if (err) throw err;
 
         close(fd, resolve);
