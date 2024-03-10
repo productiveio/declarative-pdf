8f526c3796bd6ee8431d2fe65889aea56caf2cd9
Tibor Rogulja <tibor.rogulja@productive.io>
2024-03-10 22:49:50 +0100

    update test runner

--- bin/check-canvas.cjs
+++ bin/check-canvas.cjs
@@ -1,13 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { execSync } = require('child_process');
 
 try {
   // Try to import canvas
   require('canvas');
 
-  console.log('Canvas is compiled against the current Node.js version');
+  // console.log('Canvas is compiled against the current Node.js version');
 } catch (error) {
-  if (error.message.includes('was compiled against a different Node.js version')) {
-    console.log('Canvas is compiled against a different Node.js version. Rebuilding...');
+  if (
+    error.message.includes('was compiled against a different Node.js version')
+  ) {
+    console.log(
+      'Canvas is compiled against a different Node.js version. Rebuilding...'
+    );
     execSync('npm rebuild canvas', { stdio: 'inherit' });
   } else {
     throw error;
