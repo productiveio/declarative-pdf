5640e8a72fbece21b75298acd4ea3931851d31d9
Tibor Rogulja <tibor.rogulja@productive.io>
2025-01-03 12:52:54 +0100

    cleanup index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { normalizeSetting } from '@app/utils/normalize-setting';
 import { PaperDefaults, type PaperOpts } from '@app/utils/paper-defaults';
 import HTMLAdapter, { type MinimumBrowser } from '@app/utils/adapter-puppeteer';
 import logger from '@app/models/debug-time-log';
+import { buildPages } from '@app/utils/layout/build-pages';
 
 interface DebugOptions {
   /** Do we want to log debug information */
@@ -63,58 +64,65 @@ export default class DeclarativePDF {
    */
   async generate(template: string) {
     const JOB0 = `[Σ] Total time for ${this.debug.pdfName ?? 'PDF'}`;
-    const JOB1 = '[1] Opening new tab';
-    const JOB2 = '[2] Setting content and loading html';
-    const JOB3 = '[3] Normalizing content';
-    const JOB4 = '[4] Getting document page settings from DOM';
-    const JOB5 = '[5] Build layout for every document page model';
-    const JOB6 = '[6] Processing document page models';
-    const JOB7 = '[7] Closing tab';
-    const JOB8 = '[8] Building PDF';
-    const JOBx = '[x] Closing tab after error';
-
     logger.startSession(JOB0);
     /** (re)set documentPages */
     this.documentPages = [];
 
     try {
-      /** open new tab in browser */
+      /** open a new tab in the browser */
+      const JOB1 = '[1] Opening new tab';
       logger.add(JOB1);
       await this.html.newPage();
       logger.end(JOB1);
 
-      /** send template to tab and normalize it */
+      /** send the template to the tab and normalize it */
+      const JOB2 = '[2] Setting content and loading html';
       logger.add(JOB2);
       await this.html.setContent(template);
       logger.end(JOB2);
+      const JOB3 = '[3] Normalizing content';
       logger.add(JOB3);
       await this.html.normalize();
       logger.end(JOB3);
 
       /** get from DOM index, width and height for every document-page element */
+      const JOB4 = '[4] Getting document page settings from DOM';
       logger.add(JOB4);
       await this.getDocumentPageSettings();
       logger.end(JOB4);
       /** for every document page model, get from DOM what that document-page contains */
+      const JOB5 = '[5] Build page layout and body';
       logger.add(JOB5);
       await this.buildLayoutForEachDocumentPage();
       logger.end(JOB5);
 
-      // TODO: add a check if we can return early (no layouting needed, just return body buffer)
+      /**
+       * Return early for only one document page with only a body element.
+       *
+       * This is a special case where we can return the body buffer directly
+       * because there are no headers, footers or sections to process. So,
+       * resulting PDF will be the same as the body buffer.
+       */
+      if (
+        this.documentPages.length === 1 &&
+        !this.documentPages[0].hasSections
+      ) {
+        return this.documentPages[0].body!.buffer;
+      }
 
-      /** for every document page model, process any element they might have */
+      /**
+       * We either have multiple document pages or some section elements,
+       * so we need to process them to build the final PDF.
+       */
+      const JOB6 = '[6] Process sections and build final PDF';
       logger.add(JOB6);
-      await this.processDocumentPageModels();
-      logger.end(JOB6);
-
-      /** we should have everything, time to build pdf */
-      logger.add(JOB8);
       const result = await this.buildPDF();
-      logger.end(JOB8);
+      logger.end(JOB6);
 
       return result;
     } catch (error) {
       /** cleanup - always close opened tab in the browser to avoid memory leaks */
+      const JOBx = '[x] Closing tab after error';
       logger.add(JOBx);
       await this.html.close();
       logger.end(JOBx);
@@ -127,6 +135,7 @@ export default class DeclarativePDF {
       throw error;
     } finally {
       /** cleanup - close the tab in browser */
+      const JOB7 = '[7] Closing tab';
       logger.add(JOB7);
       await this.html.close();
       logger.end(JOB7);
@@ -157,10 +166,11 @@ export default class DeclarativePDF {
     });
   }
 
+  // TODO: remove this getter
   get needsLayouting() {
-    if (!this.documentPages?.length) throw new Error('No document pages found');
+    if (!this.documentPages.length) throw new Error('No document pages found');
 
-    return this.documentPages.some((doc) => doc.layout?.hasMeta);
+    return this.documentPages.some((doc) => doc.hasSections);
   }
 
   /**
@@ -173,89 +183,37 @@ export default class DeclarativePDF {
    * information needed to build the final PDF.
    */
   private async buildLayoutForEachDocumentPage() {
-    if (!this.documentPages?.length) throw new Error('No document pages found');
+    if (!this.documentPages.length) throw new Error('No document pages found');
 
     for (const [index, doc] of this.documentPages.entries()) {
       await this.html.setViewport(doc.viewPort);
-      const settings = await this.html.getSectionSettings({ index });
+
+      let settings;
+      if (doc.hasSections) {
+        settings = await this.html.getSectionSettings({ index });
+      }
+
       await doc.createLayoutAndBody(settings);
-    }
-  }
-
-  private async processDocumentPageModels() {
-    if (!this.documentPages?.length) throw new Error('No document pages found');
-
-    for (const doc of this.documentPages) {
-      await doc.process();
     }
   }
 
   private async buildPDF() {
     if (!this.documentPages?.length) throw new Error('No document pages found');
 
-    if (
-      this.documentPages.length === 1 &&
-      !this.documentPages[0].layout!.pages?.length
-    ) {
-      // flow 1: nemamo headere i footere, imamo samo jedan body
-      // - vracamo vec postojeci buffer i izlazimo iz funkcije
-      return this.documentPages[0].body!.buffer;
-    } else {
-      // flow 2: imamo headere i/ili footere, imamo jedan body ili vise njih
-      // - kreiramo bazni doc, embedamo elemente, placeamo na stranice, vracamo buffer
-      const outputPDF = await PDFDocument.create();
+    const outputPDF = await PDFDocument.create();
 
-      for (const doc of this.documentPages) {
-        if (!doc.layout!.pages?.length) {
-          // case 1 - we only have a body, we can copy pages
-          const copiedPages = await outputPDF.copyPages(
-            doc.body!.pdf,
-            doc.body!.pdf.getPageIndices()
-          );
-          copiedPages.forEach((page) => outputPDF.addPage(page));
-        } else {
-          // case 2 - we have sections, need to place them on pages
-          for (const page of doc.layout!.pages!) {
-            const currentOutputPage = outputPDF.addPage([
-              doc.width,
-              doc.height,
-            ]);
-
-            for (const section of [
-              'background',
-              'header',
-              'footer',
-              'body',
-            ] as const) {
-              const el = page[section];
-              if (!el) continue;
-
-              const sectionPage = el.pdfPage;
-              if (!sectionPage) {
-                throw new Error(
-                  `No PDF page found for section ${section} on document page ${doc.index}`
-                );
-              }
-
-              const embeddedPage = await outputPDF.embedPage(sectionPage);
-              if (!embeddedPage) {
-                throw new Error(
-                  `Failed to embed PDF page for section ${section} on document page ${doc.index}`
-                );
-              }
-
-              currentOutputPage.drawPage(embeddedPage, {
-                width: el.width,
-                height: el.height,
-                x: el.x,
-                y: el.y,
-              });
-            }
-          }
-        }
-      }
-
-      return outputPDF.save();
+    for (const doc of this.documentPages) {
+      await buildPages({
+        documentPageIndex: doc.index,
+        pageCountOffset: doc.pageCountOffset,
+        totalPagesNumber: this.totalPagesNumber,
+        layout: doc.layout!,
+        body: doc.body!,
+        target: outputPDF,
+        html: this.html,
+      });
     }
+
+    return outputPDF.save();
   }
 }
