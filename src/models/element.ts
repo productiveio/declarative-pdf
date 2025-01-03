bc07a72c023bfea4bf281697aed5bf9b28a889a4
Tibor Rogulja <tibor.rogulja@productive.io>
2025-01-03 12:52:23 +0100

    add to elements

--- src/models/element.ts
+++ src/models/element.ts
@@ -1,23 +1,46 @@
-import type { PDFDocument } from 'pdf-lib';
+import type { PDFDocument, PDFEmbeddedPage } from 'pdf-lib';
 import type { SectionSetting } from '@app/evaluators/section-settings';
 
 interface BodyElementOpts {
   buffer: Buffer;
   pdf: PDFDocument;
+  layout: {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+  };
 }
 
-// TODO: check if we need to handle page embedding
 export class BodyElement {
   declare buffer: Buffer;
   declare pdf: PDFDocument;
+  declare layout: BodyElementOpts['layout'];
 
   constructor(opts: BodyElementOpts) {
     this.buffer = opts.buffer;
     this.pdf = opts.pdf;
+    this.layout = opts.layout;
   }
 
-  embedPageNum(num: number, targetDocument: PDFDocument) {
-    return targetDocument.embedPdf(this.pdf, [num]);
+  get x() {
+    return this.layout.x;
+  }
+
+  get y() {
+    return this.layout.y;
+  }
+
+  get width() {
+    return this.layout.width;
+  }
+
+  get height() {
+    return this.layout.height;
+  }
+
+  async embedPageIdx(targetDocument: PDFDocument, idx: number) {
+    return await targetDocument.embedPdf(this.pdf, [idx]);
   }
 }
 
@@ -25,6 +48,12 @@ interface SectionElementOpts {
   buffer: Buffer;
   pdf: PDFDocument;
   setting: SectionSetting;
+  layout: {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+  };
 }
 
 // TODO: figure out page embedding
@@ -32,23 +61,38 @@ export class SectionElement {
   declare buffer: Buffer;
   declare pdf: PDFDocument;
   declare setting: SectionSetting;
+  declare layout: SectionElementOpts['layout'];
 
-  isEmbedded: boolean = false;
+  private embeddedPage?: PDFEmbeddedPage;
 
   constructor(opts: SectionElementOpts) {
     this.buffer = opts.buffer;
     this.pdf = opts.pdf;
     this.setting = opts.setting;
+    this.layout = opts.layout;
   }
 
-  get isReusable() {
-    return (
-      this.setting.hasCurrentPageNumber || this.setting.hasTotalPagesNumber
-    );
+  get x() {
+    return this.layout.x;
   }
 
-  embedPage(targetDocument: PDFDocument) {
-    this.isEmbedded = true;
-    return targetDocument.embedPdf(this.pdf);
+  get y() {
+    return this.layout.y;
+  }
+
+  get width() {
+    return this.layout.width;
+  }
+
+  get height() {
+    return this.setting.height;
+  }
+
+  async embedPage(targetDocument: PDFDocument) {
+    if (this.embeddedPage) return this.embeddedPage;
+
+    const pages = await targetDocument.embedPdf(this.pdf);
+    this.embeddedPage = pages[0];
+    return pages[0];
   }
 }
