6c90815e93499cd47c3519558a83e5de0fdc668a
Tibor Rogulja <tibor.rogulja@productive.io>
2025-01-05 20:44:54 +0100

    add coverage report

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,14 @@ export default {
     '^@app/(.*)$': '<rootDir>/src/$1',
     '^@test/(.*)$': '<rootDir>/test/$1',
   },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/evaluators/**'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['text', 'html'],
 };
